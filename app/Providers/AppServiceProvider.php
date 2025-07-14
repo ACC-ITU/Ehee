d5286f8ea2feb162528f2804ff0dfe9a52391c21
Ubaidulla Ali <73405613+chumputys@users.noreply.github.com>
2025-07-14 21:00:09 +0500

    Microsoft OAuth implemented. fixes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
@@ -26,5 +28,10 @@ class AppServiceProvider extends ServiceProvider
         Event::listen(function (\SocialiteProviders\Manager\SocialiteWasCalled $event) {
             $event->extendSocialite('microsoft', \SocialiteProviders\Microsoft\Provider::class);
         });
+
+        Model::shouldBeStrict();
+        if (app()->isProduction()) {
+            DB::prohibitDestructiveCommands();
+        }
     }
 }
